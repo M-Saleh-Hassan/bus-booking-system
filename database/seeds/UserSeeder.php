cdc2f5b6344dc74754c58d4c359cd5f4bfab76a7
Mohamed Saleh <mohmed2778@hotmail.com>
2020-03-23 11:33:59 +0200

    add dummy test user and admin

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -12,7 +13,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        /**
+         * Add dummy user.
+         */
+        User::firstOrCreate([
+            'name' => 'test',
+            'email' => 'test@test.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(123456),
+            'remember_token' => Str::random(10),
+        ]);
+
+        /**
+         * Add dummy admin.
+         */
+        User::firstOrCreate([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(123456),
+            'remember_token' => Str::random(10),
+        ]);
+
         if(User::get()->count() < 100)
             factory(User::class, 100)->create();
+
     }
 }
